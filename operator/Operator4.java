7ca651de1d45c6087aaf31660feb87847a9f47e7
Loun <leeloun1014@gmail.com>
2026-03-28 22:31:02 +0900

    Add files via upload

---
+++ operator/Operator4.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class Operator4 {
+    static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3;
+        int sum4 = (2 * 2) + (3 * 3);
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+
+        /* 복잡한 경우 꼭 괄호 명확하게 넣어주기!
+        코드 몇자 줄여서 모호해지는 것보단
+        코드가 많더라도 명확하고 단순한 것이 유지보수하기 좋다.
+         */
+
+    }
+}
