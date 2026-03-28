554b35d7cd3f36e0582de2cc543f5f4d5774e0b1
Loun <leeloun1014@gmail.com>
2026-03-28 21:05:32 +0900

    Add files via upload

---
+++ variable/Var6.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var6 {
+
+    static void main(String[] args) {
+        int a;
+        //System.out.println(a);
+
+        /* 컴파일 에러 발생
+        변수를 선언하면 메모리상의 공간을 차지하고 사용한다.
+        그 공간에 어떤 값이 있었는지는 모른다.
+        따라서 변수 초기화를 하지 않으면 이상한 값이 출력될 수 있다.
+        문제 예방을 위해 자바는 변수 초기화를 강제한다.
+         */
+    }
+}
---
+++ variable/VarEx3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3 {
+
+    static void main(String[] args) {
+        long l = 100000000000L;
+        System.out.println(l);
+        boolean b = true;
+        System.out.println(b);
+    }
+}
